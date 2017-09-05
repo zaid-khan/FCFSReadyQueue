280b3f23cd5b64c93575aca7f2b6111d7e5e2892
ZaidKhan <zaidcomps@hotmail.com>
2017-09-05 13:31:55 +0530

    Working Program

--- SimulatorwithLL.c
+++ SimulatorwithLL.c
@@ -70,9 +70,11 @@ void ManageReadyQueue(int);
 void ManageRunning(int);
 void PrintBlockQueue();
 void PrintReadyQueue();
+void PrintRunning();
+void PrintCompleted();
 void ManageArrival(int);
-void TransferToReadyQueue(Process *, int);
-void TransferToCompletedQueue(Process *, int);
+
+
 
 
 
@@ -80,7 +82,7 @@ void TransferToCompletedQueue(Process *, int);
 //Method Declaration + Definiton for small methods
 bool CheckCompletion()
 {
-	if (readyQueue == NULL && blockQueue == NULL && Puncompleted == NULL)
+	if (readyQueue == NULL && blockQueue == NULL && running == NULL && Puncompleted == NULL)
 		return true;
 	return false;
 }
@@ -107,7 +109,7 @@ void ManageTheTime(int counter, Burst *t)
 void TransferToCPU(Process *temp, int counter)
 {
 	running = temp;
-	printf ("Process %d has started running", temp->data.pID);
+	//printf ("\nProcess P%d has started running", temp->data.pID); 
 	Burst *t = temp->data.pBurstList->next; //The 1st burst is always the IO burst so we start from 2nd burst
 	while(t->starttime != -1)
 	{
@@ -120,8 +122,50 @@ void TransferToCPU(Process *temp, int counter)
 	}
 	temp->next = NULL;
 	ManageTheTime(counter, t);
+	printf("\nProcess P%d will now start its execution.", temp->data.pID);
 }
 
+
+
+
+//------------------------------------------------------------------------------------------------
+void TransferToReadyQueue(Process *temp, int counter)
+{
+	Process *traverse;
+	if (readyQueue == NULL)
+		readyQueue = temp;
+	else
+	{
+		traverse = readyQueue;
+		while (traverse->next != NULL)
+		{
+			traverse = traverse->next;	
+		}
+		traverse->next = temp;
+	}
+	temp->next = NULL;	
+	printf ("\nProcess P%d has been added to the Ready Queue.", temp->data.pID);
+}
+
+
+//------------------------------------------------------------------------------------------------
+void TransferToCompletedQueue(Process *temp, int counter)
+{
+	Process *traverse;
+	if (Pcompleted == NULL)
+		Pcompleted = temp;
+	else
+	{
+		traverse = Pcompleted;
+		while (traverse->next != NULL)
+		{
+			traverse = traverse->next;	
+		}
+		traverse->next = temp;
+	}
+	temp->next = NULL;	
+	printf ("\nProcess P%d is finished.", temp->data.pID);	
+}
 
 
 
@@ -240,6 +284,7 @@ void AddToBlockQueue(Process *temp, int counter)
 	}
 	ManageTheTime(counter, t);
 	temp->next = NULL;
+	printf("\nProcess P%d has been added to the block Queue.", temp->data.pID);
 }
 
 
@@ -248,18 +293,26 @@ void AddToBlockQueue(Process *temp, int counter)
 //------------------------------------------------------------------------------------------------
 void ManageArrival(int counter)
 {
-	while (Puncompleted != NULL)
+	if (Puncompleted != NULL)
 	{
 		Process *temp = Puncompleted;
-		if (temp->data.aTime == counter)
+		while (temp != NULL)
 		{
-			Puncompleted = temp->next;
-			printf ("\nProcess P%d has arrived at %d", temp->data.pID, counter);
-			AddToBlockQueue(temp, counter);
-			//break; //Assuming two processes won't arrive at the same time, i.e. their arrival time differs.
+			if (temp->data.aTime == counter)
+			{
+				Puncompleted = temp->next; //We're removing the current process from the initial list i.e. Puncompleted, so the Puncompleted 
+				//head will point at the next element in the list
+				printf ("\nProcess P%d has arrived at %dms", temp->data.pID, counter);
+				AddToBlockQueue(temp, counter);
+				temp = Puncompleted; //temp will now point at the next element in the list as the current element has been removed
+				//break; //Assuming two processes won't arrive at the same time, i.e. their arrival time differs.
+			}
+			else
+			{
+				printf("\nProcess P%d has not yet arrived at %dms", temp->data.pID, counter);
+				temp = temp->next;
+			}
 		}
-		else
-			printf("\nProcess P%d has not yet arrived at %d", temp->data.pID, counter);
 	}
 }
 
@@ -281,16 +334,16 @@ void ManageBlockQueue(int counter)
 			while (t != NULL)
 			{
 				tomove = false; //Initially tomove set to false
-				if ((t->starttime != -1) && ((t->endtime + 1) >= counter))
+				if ((t->starttime != -1) && (t->endtime >= counter)) //Old t->endtime + 1 >= counter
 				{	
 					
-					if (t->starttime < counter && t->endtime > counter)
+					if (t->starttime < counter && ((t->endtime - 1) > counter)) //Old - t->endtime > counter
 						printf("\nProcess P%d is currently in the Block Queue doing I/O.", temp->data.pID);
-					else if (t->starttime == counter)
-						printf("\nProcess P%d has just been added to the Block Queue for doing I/O.", temp->data.pID);
-					else if (t->endtime == counter)
+					//else if (t->starttime == counter)
+					//	printf("\nProcess P%d has just been added to the Block Queue for doing I/O.", temp->data.pID);
+					else if (t->endtime - 1 == counter) //Old - "else if (t->endtime == counter)"
 						printf("\nProcess P%d is finishing up doing I/O in the BlockQueue.", temp->data.pID);
-					else if (t->endtime + 1 == counter) //counter has exceeded the endtime by 1
+					else if (t->endtime == counter) //counter has exceeded the endtime by 1. Old - "else if (t->endtime + 1 == counter)"
 					{
 						printf("\nProcess P%d will now exit the BlockQueue.", temp->data.pID);
 						
@@ -315,17 +368,20 @@ void ManageBlockQueue(int counter)
 						break; //No point in further checking next I/O burst of that process so break.
 					}
 				}
-				t = (t->next)->next; //Alternate Burst
+				if (t->next != NULL) //IF the next burst is not NULL then the burst after the next burst won't be NULL because IO burst will be the final burst
+					t = (t->next)->next; //Alternate Burst
+				else
+					t = NULL; //IF the next burst is NULL then the burst after the next burst will be NULL too
 			}
 			if (tomove)
-				temp = para; //Next process
+				blockQueue = temp = para; //Next process
 			else
 				temp = temp->next; //The Normal Next Process
 				
 		}
 	}
-	else
-		printf("\nBlock Queue is empty");
+	//else
+	//	printf("\nBlock Queue is empty");
 }
 
 
@@ -343,17 +399,18 @@ void ManageReadyQueue(int counter)
 		Process *para;  
 		while (temp != NULL)
 		{
-			Burst *t = temp->data.pBurstList; //1st burst
+			Burst *t = temp->data.pBurstList; //1st burst which is IO burst
 			while (t != NULL)
 			{
 				tomove = false; //Initially tomove set to false
-				if ((t->starttime != -1) && (t->endtime < counter)) //IOBurst has been completed of that process ......odd bursts
-				{	
-					if ((t->next != NULL) && ((t->next)->starttime != -1)) //There "exists" a CPU Burst after the 
-					{												       //IO burst which has not been started
+				if ((t->starttime != -1) && (t->endtime <= counter)) //IOBurst has been completed of that process ......odd bursts
+				{													 //Old - t->endtime < counter
+					if ((t->next != NULL) && ((t->next)->starttime == -1)) //There "exists" a CPU Burst after the 
+					{												       //IO burst which has NOT been started
 						if(isCPUFree())
 						{
-							para = temp->next;//para is here
+							para = temp->next; //para is here
+							printf("\nCPU is free right now.");
 							TransferToCPU(temp, counter);
 							tomove = true;
 							 
@@ -362,7 +419,7 @@ void ManageReadyQueue(int counter)
 							//will be part of another list.
 						
 							//**Remember we're removing processes from list we're traversing**
-							printf("\nProcess P%d will now get executed by the CPU.", temp->data.pID);
+							
 							break;
 						}
 						else
@@ -374,16 +431,19 @@ void ManageReadyQueue(int counter)
 					}
 					
 				}
-				t = (t->next)->next; //Next IO Burst
+				if (t->next != NULL) //IF the next burst is not NULL then the burst after the next burst won't be NULL because IO burst will be the final burst
+					t = (t->next)->next; //Next IO Burst
+				else
+					t = NULL; //IF the next burst is NULL then the burst after the next burst will be NULL too
 			}
 			if (tomove)
-				temp = para; //Next process after removal of the current process
+				readyQueue = temp = para; //Next process after removal of the current process
 			else
 				temp = temp->next; //The Normal Next Process		
 		}
 	}		
-	else
-		printf("\nReady Queue is empty");
+	//else
+	//	printf("\nReady Queue is empty");
 }
 
 
@@ -398,30 +458,36 @@ void ManageRunning(int counter)
 		Burst *t = temp->data.pBurstList->next; //Atleast 1 CPU Burst exists for each process so we can directly point towards it
 		do
 		{
-			if ((t->starttime != -1) && ((t->endtime + 1) >= counter)) //The second after the last second of the CPU burst
-			{														   //should be greater than current time.
-				if (t->starttime < counter && t->endtime > counter)
+			if ((t->starttime != -1) && (t->endtime >= counter)) //The last second of the CPU burst should be greater or equal to the
+			{													  // current time.	   
+				if (t->starttime < counter && ((t->endtime - 1) > counter))
 					printf("\nProcess P%d is currently executing.", temp->data.pID);
-				else if (t->starttime == counter)
-					printf("\nProcess P%d has just started execution.", temp->data.pID);
-				else if (t->endtime == counter)
+				//else if (t->starttime == counter)
+				//	printf("\nProcess P%d has just started execution.", temp->data.pID);
+				else if (t->endtime - 1 == counter) //Old - else if (t->endtime == counter)
 					printf("\nProcess P%d is finishing execution.", temp->data.pID);
-				else if (t->endtime + 1 == counter) //counter has exceeded the endtime by 1
+				else if (t->endtime == counter) //counter has exceeded the endtime by 1 Old - else if (t->endtime + 1 == counter)
 				{
 					printf("\nProcess P%d execution is over. It will free the CPU now.", temp->data.pID);
 					running = NULL;
 					AddToBlockQueue(temp, counter);
+					ManageReadyQueue(counter); 
+					/*I'm calling ManageReady() again, because if a process has completed execution at current time, 
+					there may have been be a process at current time waiting at the ReadyQueue which got rejected 
+					in ManageReadyQueue() before(remember ManageReadyQueue is called before this method ManageRunning())
+					because of the current process which got removed here
+					and got admitted to the Block Queue. */
 					break; //Execution is over, now time for IO. Remember a process ALWAYS ENDS WITH A IO BURST
 				}
 			}
-			if ((t->next)->next != NULL) //IF 4th...6th....8th(ie the 2nd...4th...6th.... CPU burst) burst is not null
+			if ((t->next)->next != NULL) //IF 4th...6th....8th(ie the 2nd...4th...6th.... CPU burst) burst is not null 
 				t = (t->next)->next; //Alternate Burst
 			else
 				t = t->next->next; // OR t = NULL;
 		}while (t != NULL);
 	}
-	else
-		printf("\nNo process getting executed currently.");
+	//else
+	//	printf("\nNo process getting executed currently.");
 }
 
 
@@ -430,12 +496,16 @@ void ManageRunning(int counter)
 void PrintReadyQueue()
 {
 	Process *temp = readyQueue;
-	printf ("\n------The Ready Queue-------");
+	int count = 0;
+	printf ("\n\n------The Ready Queue-------");
 	while (temp != NULL)
 	{
-		printf ("\nProcess P%d", temp->data.pID);
+		++count;
+		printf ("\n\t%d. Process P%d", count, temp->data.pID);
 		temp = temp->next;
 	}
+	if(count == 0)
+		printf("\n\tQueue is empty");
 	printf ("\n----------------------------");
 }
 
@@ -444,74 +514,84 @@ void PrintReadyQueue()
 void PrintBlockQueue()
 {
 	Process *temp = blockQueue;
-	printf ("\n------The Block Queue-------");
+	int count = 0;
+	printf ("\n\n------The Block Queue-------");
 	while (temp != NULL)
 	{
-		printf ("\nProcess P%d", temp->data.pID);
+		++count;
+		printf ("\n\t%d. Process P%d", count, temp->data.pID);
 		temp = temp->next;
 	}
+	if(count == 0)
+		printf("\n\tQueue is empty");
 	printf ("\n----------------------------");
 }
 
 
-//------------------------------------------------------------------------------------------------
-
-
 
 //------------------------------------------------------------------------------------------------
-void TransferToReadyQueue(Process *temp, int counter)
+void PrintCompleted()
 {
-	Process *traverse;
-	if (readyQueue == NULL)
-		readyQueue = temp;
-	else
+	Process *temp = Pcompleted;
+	int count = 0;
+	printf ("\n\n------The Completed List-------");
+	while (temp != NULL)
 	{
-		traverse = readyQueue;
-		while (traverse->next != NULL)
-		{
-			traverse = traverse->next;	
-		}
-		traverse->next = temp;
+		++count;
+		printf ("\n\t%d. Process P%d", count, temp->data.pID);
+		temp = temp->next;
 	}
-	temp->next = NULL;	
-	printf ("Process %d has been added to the Ready Queue.", temp->data.pID);	
+	if(count == 0)
+		printf("\nNo Process has been completed by now.");
+	printf ("\n---------------------------------");
 }
 
+
 //------------------------------------------------------------------------------------------------
-void TransferToCompletedQueue(Process *temp, int counter)
+void PrintRunning()
 {
-	Process *traverse;
-	if (Pcompleted == NULL)
-		Pcompleted = temp;
+	Process *temp = running;
+	printf ("\n\n------Under CPU Execution-------");
+	if (temp != NULL)
+		printf ("\n\t>Process P%d<", temp->data.pID);
 	else
-	{
-		traverse = Pcompleted;
-		while (traverse->next != NULL)
-		{
-			traverse = traverse->next;	
-		}
-		traverse->next = temp;
-	}
-	temp->next = NULL;	
-	printf ("Process %d has finished.", temp->data.pID);	
+		printf ("\nNo process is currently under execution.");
+	printf ("\n--------------------------------");
 }
+
+
+
 
 //------------------------------------------------------------------------------------------------
 int main()
 {
-	
+	char ch;
 	Puncompleted = CreateElements();
 	printLL(Puncompleted);
 	
+	
+	
+	printf("\n-----------------------------------------------------------------------------");
+	printf("\nTO STOP EXECUTION PRESS THE ; KEY AND THEN PRESS ENTER.");
+	printf("\nTO CONTINUE JUST PRESS ENTER.");
+	printf("\nASSUMPTION: STS takes a negligible amount of time to execute.");
+	printf("\n-----------------------------------------------------------------------------");
 	for(int counter = 0;/*Loops breaks from the body itself so this part is left empty*/;++counter)
-	{
+	{   
+		printf("\n\n\n\n================Counter %dms-%0.2fms======================================", counter, (float)counter + 0.99);
+		
 		ManageArrival(counter);
 		ManageBlockQueue(counter); 
 		ManageReadyQueue(counter);
 		ManageRunning(counter);
+		printf("\n-------------------------------------------------------------------------------\n");
 		PrintBlockQueue();
 		PrintReadyQueue();
-		if(CheckCompletion())
+		PrintRunning();
+		PrintCompleted();
+		printf("\n========================================================================\n");
+		scanf("%c", &ch);
+		if(CheckCompletion() || (ch == ';'))
 			break;
 	}
 	
